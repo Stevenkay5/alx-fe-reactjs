fa428d34112db2bfc8bd5eec1f895e6736b06378
Steven Kayiranga <stevenkayiranga5@gmail.com>
2026-01-22 23:24:09 +0200

    my-company

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ padding: '10px', textAlign: 'center' }}>
+      <p>© 2026 My Company</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: '20px',
+        backgroundColor: '#333',
+        display: 'flex',
+        gap: '15px',
+        textAlign: 'center',
+        
+      }}
+    >
+      <Link style={{ color: '#fff' }} to="/">Home</Link>
+      <Link style={{ color: '#fff' }} to="/about">About</Link>
+      <Link style={{ color: '#fff' }} to="/services">Services</Link>
+      <Link style={{ color: '#fff' }} to="/contact">Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
