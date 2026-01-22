961817461ca34ee124356b70ef3c808988af89ee
Steven Kayiranga <stevenkayiranga5@gmail.com>
2026-01-22 21:19:48 +0200

    Counter App

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -18,6 +19,8 @@ function App() {
         age="25"
         bio="Loves hiking and photography"
         />
+        <h1>Counter App</h1>
+        <Counter/>
       <WelcomeMessage />
       <Header/>
       <MainContent/>
