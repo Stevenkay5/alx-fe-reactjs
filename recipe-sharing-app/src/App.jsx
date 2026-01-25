3556d95a20eb7a502a67b755a0e3e97de669c003
Steven Kayiranga <stevenkayiranga5@gmail.com>
2026-01-25 22:31:22 +0200

    Details, Edit & Delete

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,18 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <BrowserRouter>
     <div style={{ maxWidth: '600px', margin: '40px auto', padding: '20px' }}>
       <h1> Recipe Sharing App</h1>
-
-      <AddRecipeForm />
-      <RecipeList />
+      <Routes>
+        <Route
+        path="/"
+        element={
+          <>
+          <AddRecipeForm />
+          <RecipeList />
+          </>
+        }
+        />
+        <Route path="/recipes/:id" element={<RecipeDetails/>} />
+      </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
---
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -0,0 +1,21 @@
+import { useRecipeStore } from '../store/recipeStore';
+
+const DeleteRecipeButton = ({ recipeId, onDelete }) => {
+  const deleteRecipe = useRecipeStore((state) => state.deleteRecipe);
+
+  const handleDelete = () => {
+    deleteRecipe(recipeId);
+    onDelete();
+  };
+
+  return (
+    <button
+      onClick={handleDelete}
+      style={{ backgroundColor: 'red', color: 'white', marginTop: '10px' }}
+    >
+      Delete Recipe
+    </button>
+  );
+};
+
+export default DeleteRecipeButton;
